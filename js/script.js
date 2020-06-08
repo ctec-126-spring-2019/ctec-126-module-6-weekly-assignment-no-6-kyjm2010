6b76276a96e55bd004ad2cdd97b3e3e9bb69ddaa
Kyle McDonald <kyjm2010@gmail.com>
2020-06-08 07:12:26 -0700

    Done ready for grading by Bruce

--- js/script.js
+++ js/script.js
@@ -6,66 +6,58 @@
 * This includes the code that you write and the code that was provided.
 */
 
+//class that builds "playing cards" using a constructor which passes in variables that are assigned to properties
 class PlayingCard {
     constructor(element, face, suit) {
-        /*
-        Create properties for:
-        - element
-        - suit
-        - face
-        - img (set this to `img/${face}_of_${suit}.png`)
-        - state (set this to 0)
-        */
+       this.element = element
+       this.face = face
+       this.suit = suit
+       this.img = `img/${face}_of_${suit}.png`
+       this.state = 0
 
-        // your code goes here (remove this comment once you have added your code)
-
+//each card has an event listener which is triggered when the card is clicked. This will either display the card image or its back
         this.element.addEventListener('click', () => {
-            /*
-            - The event listener should be for a click event
-            - The event listener should have logic to switch out the this.element.src
-            - It should also change the state if the card is flipped (this.state 0 or 1)
-            - To show the back of the card use 'img/back.png'
-            */
-
-            // your code goes here (remove this comment once you have added your code)
+            if(this.state == 0){
+                this.element.src = this.img
+                this.state = 1
+            } else if(this.state == 1){
+                this.element.src = 'img/back.png'
+                this.state = 0
+            }
         })
     }
 
+    //function that displays a cards face
     showFaces() {
         this.element.src = this.img
     }
-
+    //function that displays a cards back
     showBacks() {
         this.element.src = 'img/back.png'
     }
 }
 
 function createCardImage() {
-    /*
-    - Create a constant named img and have it create a new img element
-    - Set the src property of the img to 'img/back.png'
-    - return the img
-    */
-
-    // your code goes here (remove this comment once you have added your code)
+    const img = document.createElement('img')
+        img.src = 'img/back.png'
+        return img
+    
 }
-
+//adds each card to the container div
 function displayDeck() {
-    /*
-    - Create a loop that iterates through each card in the deck array
-    - in the loop, append the card.element to the container
-    - Use a forEach with an arrow function
-    */
-
-    // your code goes here (remove this comment once you have added your code)
+    deck.forEach(card => {
+        container.appendChild(card.element)
+    })
 }
 
+//shuffles the deck 1000 times
 function shuffleDeck() {
     for (let i = 0; i < 1000; i++) {
         deck.sort(() => Math.random() - 0.5)
     }
 }
 
+//function that removes a card from the front of the current deck array
 function removeCard() {
     if (deck.length != 0) {
         card = document.querySelector('img')
@@ -77,24 +69,21 @@ function removeCard() {
     }
 }
 
+//nested loops that create a deck of playing cards 13 cards for each of the 4 suits 
 function buildDeck() {
     const suits = ['hearts', 'spades', 'diamonds', 'clubs']
     const faces = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king']
 
     suits.forEach(suit => {
         faces.forEach(face => {
-            /*
-            - Call the createdCardImage() function and assign the return img element to a variable named image
-            - Set the id attribute of the image to `${face}_of_${suit}.png`
-            - Use the .push method to push a new PlayingCard object into the deck array
-            - Do the .push and object creation in a single statement
-            */
-
-            // your code goes here (remove this comment once you have added your code)
+            let image = createCardImage()
+            image.setAttribute('id', `${face}_of_${suit}.png`)
+            deck.push(new PlayingCard(image, face, suit))
         })
     })
 }
 
+//a function that clears messages displayed when the user interracts with the interface. Called throughout with the setTimeout function
 function clearActions() {
     actions.innerHTML = ''
 }
@@ -113,7 +102,9 @@ shuffleBtn.addEventListener('click', () => {
     actions.innerHTML = 'The deck of cards has been shuffled.'
     container.innerHTML = ''
     shuffleDeck()
+    //creates a half second delay to simulate the deck being reloaded even though it happens instantly
     setTimeout(displayDeck, 500)
+    //removes the displayed message after 5 seconds
     setTimeout(clearActions, 5000)
 })
 
@@ -123,6 +114,7 @@ removeBtn.addEventListener('click', () => {
     setTimeout(clearActions, 5000)
 })
 
+//event listener that makes the deck array empty, clears the html of the container div then propogates it by calling the buildDeck function
 newDeckBtn.addEventListener('click', () => {
     actions.innerHTML = 'A new deck of cards has been created.'
     deck = []
